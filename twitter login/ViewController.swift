87311b59cbaa941887a68c3e56a4133dccabb457
PM Academy 3 <pmacademy3@PMs-Mac-mini-2.local>
2018-07-18 17:04:07 +0700

    go home with bugs

--- twitter login/ViewController.swift
+++ twitter login/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var oauthswift: OAuthSwift?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         AuthenticationManager.shared.googleDelegate = self
